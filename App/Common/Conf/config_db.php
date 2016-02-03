ba8b2bf203e08e3b1c7c388ab6ec6a06b42f1a4f
499478785 <499478785@qq.com>
2016-02-03 16:06:27 +0800

    佳美源码

--- App/Common/Conf/config_db.php
+++ App/Common/Conf/config_db.php
@@ -5,10 +5,10 @@
  */
 return array(
     'DB_TYPE' => 'mysql', //数据库类型
-    'DB_HOST' => '192.168.0.112', //数据库主机
+    'DB_HOST' => '120.76.73.46', //数据库主机
     'DB_NAME' => 'jm', //数据库名称
-    'DB_USER' => 'root', //数据库用户名
-    'DB_PWD' => '', //数据库密码
+    'DB_USER' => 'jmyz', //数据库用户名
+    'DB_PWD' => 'jmyz*2016', //数据库密码
     'DB_PORT' => '3306', //数据库端口
     'DB_PREFIX' => 'tuzi_', //数据库前缀
     "AUTHCODE" => '5cP3UOQa4VzEHym6Gk', //密钥
