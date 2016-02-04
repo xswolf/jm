78234e448da3dbe8b37db594f4a361cb8e54cc72
499478785 <499478785@qq.com>
2016-02-04 10:59:18 +0800

    佳美源码

---
+++ App/Manage/Controller/OrderController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/2/4
+ * Time: 10:55
+ */
+
+namespace Manage\Controller;
+
+
+class OrderController extends CommonController
+{
+
+    /**
+     * 预约用户列表
+     */
+    public function lists(){
+
+    }
+
+    /**
+     * 获取预约信息
+     * @param $id
+     */
+    public function getOrderById($id){
+
+    }
+
+    /**
+     * 删除预约信息
+     * @param $id
+     */
+    public function delOrderById($id){
+
+    }
+}
