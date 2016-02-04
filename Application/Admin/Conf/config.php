2ed17de38d71a4077f127ec8b94d1726b9a8fd6e
sg <825512123@qq.com>
2016-02-04 16:07:47 +0800

    后台登录

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -3,4 +3,5 @@ return array(
 	//'配置项'=>'配置值'
     'LAYOUT_ON'=>true,
     'LAYOUT_NAME'=>'layout/main',
+    'LOGIN_INFO'=>'userinfo',
 );
--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -16,17 +16,25 @@ class UserController extends Controller
 {
     /**
      * 用户登录
-     * @param $username
-     * @param $password
      */
-    public function login($username = '', $password = ''){
+    public function login(){
+        //判断否非已经登陆
+        if(session(C('LOGIN_INFO'))) {
+            $this->redirect('Admin/Index/index');
+        }
+        //登录判断
         if ($_POST) {
             $user = new User();
-            $userInfo = $user->login($username , $password);
+            $username = I('user_name');
+            $password = I('user_pass');
+            $userInfo = $user->login($username);
             if ($userInfo['password'] === md5($password) ) {
                 // 登录成功
+                $this->redirect('Admin/Index/index');
             } else {
                 // 登录失败
+                session(C('LOGIN_INFO'),null);//删除登录信息
+                $this->error('登录失败!');
             }
         }
         $this->display();
@@ -36,7 +44,8 @@ class UserController extends Controller
      * 用户退出
      */
     public function loginOut(){
-
+        session(C('LOGIN_INFO'),null);//删除用户信息
+        $this->redirect('Admin/User/Login');//返回至登录页面
     }
 
 }
