af2e8b4d7313b648914c95870ade534b7f31b5a7
499478785 <499478785@qq.com>
2016-02-04 11:44:27 +0800

    佳美源码

--- App/Common/Conf/config_theme.php
+++
@@ -1,7 +0,0 @@
-<?php 
- return array(
-	'DEFAULT_THEME__HOME'=>'Default',
-	'DEFAULT_THEME__MOBILE'=>'Default',
-'DEFAULT_THEME__MANAGE'=>'Default',
-);
-?>
--- App/Site/Common/function.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/2/4/0004
- * Time: 10:20
- */
