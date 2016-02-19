0a052550e3a9e9831760ca81dbc56ed29aa68960
499478785 <499478785@qq.com>
2016-02-19 18:14:03 +0800

    t

--- Public/Home/js/main.js
+++ Public/Home/js/main.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/2/3.
  */
 require.config({
-    urlArgs: "bust=" + (new Date()).getTime(), // 清除缓存
+    //urlArgs: "bust=" + (new Date()).getTime(), // 清除缓存
     baseUrl: '/Public/Home/js',
     paths: {
         jquery: 'jquery',
