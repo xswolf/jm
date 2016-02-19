9448cd34ec5d4dc1d2b1caf03bbfb0f7d14a98b6
Administrator <570434734@qq.com>
2016-02-19 18:09:24 +0800

    nsk

--- Public/Home/js/main.js
+++ Public/Home/js/main.js
@@ -5,19 +5,12 @@ require.config({
     urlArgs: "bust=" + (new Date()).getTime(), // 清除缓存
     baseUrl: '/Public/Home/js',
     paths: {
+        jquery: 'jquery',
         fullpage: 'plugin/jquery/jquery.fullpage.min',
         excoloSlider: 'plugin/jquery/jquery.excoloSlider',
         adapt: 'plugin/jquery/adapt.min'
     }
 });
-
-define(['jquery','index'],
-    function (jquery,index) {
-            index.init();
-            index.loadImgSlider();
-            index.loadMenu();
-    });
-
 
 
 
